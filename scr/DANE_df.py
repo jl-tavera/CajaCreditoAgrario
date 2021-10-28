c5890327fbf1eeb080f708ce4f338c4c86419512
jl-tavera <jl.tavera@uniandes.edu.co>
2021-10-28 11:04:12 -0500

    diff

--- scr/DANE_df.py
+++ scr/DANE_df.py
@@ -6,15 +6,24 @@ import sys
 
 year_1952 = f.load_year('DANE/1952.csv')
 year_1953 = f.load_year('DANE/1953.csv')
+year_1954 = f.load_year('DANE/1954.csv')
 
-# year_1952 = f.name_correction(year_1952)
-# year_1952 = f.number_correction(year_1952)
-# year_1952 = f.check_total(year_1952)
+year_1952 = f.name_correction(year_1952)
+year_1952 = f.number_correction(year_1952)
+year_1952 = f.check_total(year_1952)
 
-# mun_1952 = f.get_mun(year_1952)
+mun_1952 = f.get_mun(year_1952)
 
-year_1953 = f.name_correction(year_1953)
+# year_1953 = f.name_correction(year_1953)
 # year_1953 = f.number_correction(year_1953)
 # year_1953 = f.check_total(year_1953)
 
 # mun_1953 = f.get_mun(year_1953)
+
+year_1954 = f.name_correction(year_1954)
+year_1954 = f.number_correction(year_1954)
+year_1954 = f.check_total(year_1954)
+
+mun_1954 = f.get_mun(year_1954)
+
+diff = f.compare_mun(mun_1952, mun_1954)
