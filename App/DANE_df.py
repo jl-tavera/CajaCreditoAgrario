7b6b96c375ad347fdfe23cb3272282e6aeb38923
jl-tavera <jl.tavera@uniandes.edu.co>
2021-10-29 09:45:17 -0500

    PEP 8

--- App/DANE_df.py
+++ App/DANE_df.py
@@ -1,7 +1,5 @@
 #%%
-import functions as f
-import pandas as pd
-
+import functions as fx
 '''
 DATA PREPARATION
 '''
@@ -10,68 +8,71 @@ DATA PREPARATION
 1952
 '''
 
-year_1952 = f.load_year('DANE/1952.csv')
+year_1952 = fx.loadYearCSV('DANE/1952.csv')
 
-year_1952 = f.name_correction(year_1952)
-year_1952 = f.number_correction(year_1952)
-year_1952 = f.check_total(year_1952)
+year_1952 = fx.nameCorrection(year_1952)
+year_1952 = fx.numberCorrection(year_1952)
+year_1952 = fx.checkTotal(year_1952)
 
-mun_1952 = f.get_names(year_1952, 'Oficina')
+mun_1952 = fx.getNames(year_1952, 'Oficina')
 
-f.export(year_1952, '1952_clean')
+fx.export(year_1952, '1952_clean')
 
 '''
 1953
 '''
 
-year_1953 = f.load_year2('DANE/1953.xlsx')
+year_1953 = fx.loadYearXLSX('DANE/1953.xlsx')
 
-year_1953 = f.number_correction(year_1953)
-year_1953 = f.check_total(year_1953)
+year_1953 = fx.numberCorrection(year_1953)
+year_1953 = fx.checkTotal(year_1953)
 
-mun_1953 = f.get_names(year_1953, 'Oficina')
-mun_1953 = f.format_name(mun_1953)
+mun_1953 = fx.getNames(year_1953, 'Oficina')
+mun_1953 = fx.formatName(mun_1953)
 
-dep_1953 = f.get_names(year_1953, 'Departamento')
-dep_1953 = f.format_name(dep_1953)
+dep_1953 = fx.getNames(year_1953, 'Departamento')
+dep_1953 = fx.formatName(dep_1953)
 
 year_1953 = year_1953.drop('Oficina', 1)
 year_1953 = year_1953.drop('Departamento', 1)
 
-year_1953= f.assign(year_1953, 'Oficina', mun_1953)
-year_1953= f.assign(year_1953, 'Departamento', dep_1953)
+year_1953 = fx.assign(year_1953, 'Oficina', mun_1953)
+year_1953 = fx.assign(year_1953, 'Departamento', dep_1953)
 
-year_1953 = f.rearrange(year_1953, 'Oficina', 0)
-year_1953 = f.rearrange(year_1953, 'Departamento', 1)
+year_1953 = fx.rearrange(year_1953, 'Oficina', 0)
+year_1953 = fx.rearrange(year_1953, 'Departamento', 1)
 
-f.export(year_1953, '1953_clean')
+fx.export(year_1953, '1953_clean')
 
 '''
 1954
 '''
 
-year_1954 = f.load_year('DANE/1954.csv')
+year_1954 = fx.loadYearCSV('DANE/1954.csv')
 
-year_1954 = f.name_correction(year_1954)
-year_1954 = f.number_correction(year_1954)
-year_1954 = f.check_total(year_1954)
+year_1954 = fx.nameCorrection(year_1954)
+year_1954 = fx.numberCorrection(year_1954)
+year_1954 = fx.checkTotal(year_1954)
 
-mun_1954 = f.get_names(year_1954, 'Oficina')
+mun_1954 = fx.getNames(year_1954, 'Oficina')
 
-f.export(year_1954, '1954_clean')
+fx.export(year_1954, '1954_clean')
 
 '''
 1955
 '''
 
-year_1955 = f.load_year('DANE/1955.csv')
+year_1955 = fx.loadYearCSV('DANE/1955.csv')
 
-year_1955 = f.name_correction(year_1955)
-year_1955 = f.number_correction(year_1955)
-year_1955 = f.check_total(year_1955)
+year_1955 = fx.nameCorrection(year_1955)
+year_1955 = fx.numberCorrection(year_1955)
+year_1955 = fx.checkTotal(year_1955)
 
-mun_1955 = f.get_names(year_1955, 'Oficina')
+mun_1955 = fx.getNames(year_1955, 'Oficina')
 
-f.export(year_1955, '1955_clean')
+fx.export(year_1955, '1955_clean')
 
-
+#    (_    ,_,    _)
+#    / `'--) (--'` \
+#   /  _,-'\_/'-,_  \
+#  /.-'     "     '-.\
--- App/functions.py
+++ App/functions.py
@@ -1,64 +1,67 @@
 #%%
-import config as cf 
+import config as cf
 import pandas as pd
-import re
-from unicodedata import normalize
 
 '''
 LOADING FUNCTIONS
 '''
 
-def load_year(path):
-    route = cf.data_dir.replace('/App', '') + path 
+
+def loadYearCSV(path):
+
+    route = cf.data_dir.replace('/App', '') + path
     year = pd.read_csv(route)
+    return year
 
-    return year 
 
-def load_year2(path):
-    route = cf.data_dir.replace('/App', '') + path 
+def loadYearXLSX(path):
+
+    route = cf.data_dir.replace('/App', '') + path
     year = pd.read_excel(route)
+    return year
 
-    return year 
 
 '''
 FORMAT & CLEANING FUNCTIONS
 '''
 
-def format_numbers(df, col): 
-    for i, row in df.iterrows(): 
-        row[col] =  row[col].replace('.', '')
-        row[col] =  row[col].replace(',', '')
-        row[col] =  row[col].replace('•', '')
-        row[col] =  row[col].replace('%', '')
-        row[col] =  row[col].replace(':', '')
-        row[col] =  row[col].replace(' ', '')
-        row[col] =  row[col].replace('I', '1')
-        row[col] =  row[col].replace('l', '1')
-  
+
+def formatNumbers(df, col):
+    for i, row in df.iterrows():
+        row[col] = row[col].replace('.', '')
+        row[col] = row[col].replace(',', '')
+        row[col] = row[col].replace('•', '')
+        row[col] = row[col].replace('%', '')
+        row[col] = row[col].replace(':', '')
+        row[col] = row[col].replace(' ', '')
+        row[col] = row[col].replace('I', '1')
+        row[col] = row[col].replace('l', '1')
+
     return df
 
 
-def number_correction(df):
+def numberCorrection(df):
 
     col_names = ['Oficina', 'Departamento']
 
-    for col in df.columns: 
-         df[col] = df[col].apply(str)
+    for col in df.columns:
+        df[col] = df[col].apply(str)
 
-    for col in df.columns: 
+    for col in df.columns:
         if col not in col_names:
-            format_numbers(df, col)
+            formatNumbers(df, col)
 
-    for col in df.columns: 
-       if col not in col_names:
-         df[col] = pd.to_numeric(df[col])
+    for col in df.columns:
+        if col not in col_names:
+            df[col] = pd.to_numeric(df[col])
 
     return df
 
-def name_correction(df): 
+
+def nameCorrection(df):
 
     col_names = ['Oficina', 'Departamento']
-    for name in col_names: 
+    for name in col_names:
         for i, row in df.iterrows():
             row[name] = row[name].replace('.', '')
             row[name] = row[name].replace(',', '')
@@ -71,8 +74,8 @@ def name_correction(df):
             row[name] = row[name].replace('í', 'i')
             row[name] = row[name].replace('ó', 'o')
             row[name] = row[name].replace('ú', 'u')
-            
-            if row[name][0] == ' ': 
+
+            if row[name][0] == ' ':
                 row[name] = row[name][1:]
 
             lower = row[name].lower()
@@ -81,40 +84,33 @@ def name_correction(df):
     return df
 
 
-def str_correction(df): 
-    for i, row in df.iterrows():
-        s = row['Oficina']
-        s = re.sub( 
-            r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
-            normalize( "NFD", s), 0, re.I
-        )
-        s = normalize( 'NFC', s)
-        row['Oficina'].lower()
-        
+def checkTotal(df):
+    df['Check - Valor'] = (
+        (df['CP - Valor'] + df['MP - Valor']) - df['T - Valor'])
+    df['Check - Numero'] = (
+        (df['CP - Numero'] + df['MP - Numero']) - df['T - Numero'])
     return df
 
-def check_total(df):  
-    df['Check - Valor'] = ((df['CP - Valor'] + df['MP - Valor']) - df['T - Valor'] )
-    df['Check - Numero'] = ((df['CP - Numero'] + df['MP - Numero']) - df['T - Numero'] )
-    return df
 
-def lower(df): 
+def lower(df):
 
     col_names = ['Oficina', 'Departamento']
-    for name in col_names: 
+    for name in col_names:
         df[name].str.lower()
     return df
 
-def get_names(df, col): 
+
+def getNames(df, col):
     names = []
     for i, row in df.iterrows():
-        names.append(row[col] )
+        names.append(row[col])
 
     return names
 
-def format_name(lst): 
+
+def formatName(lst):
     names = []
-    for i in lst: 
+    for i in lst:
         i = i.replace('.', '')
         i = i .replace(',', '')
         i = i .replace('•', '')
@@ -132,38 +128,46 @@ def format_name(lst):
 
     return names
 
+
 def assign(df, name, lst):
-    df.loc[:, name] = lst 
+    df.loc[:, name] = lst
 
     return df
 
-def rearrange(df, name, pos): 
+
+def rearrange(df, name, pos):
     first_col = df.pop(name)
     df.insert(pos, name, first_col)
 
     return df
 
-'''
-CLEAN DATABASES
-'''
 
 '''
 COMPARE FUNCTIONS
 '''
-def compare_mun(lst1, lst2): 
+
+
+def compareMun(lst1, lst2):
     diff = []
-    for i in lst1: 
-        if i not in lst2: 
+    for i in lst1:
+        if i not in lst2:
             diff.append(i)
-    return diff 
+    return diff
 
 
 '''
 EXPORT FUNCTIONS
 '''
 
-def export(df, name): 
-    route = cf.export_dir.replace('/App', '')
-    df.to_csv( route + str(name)+ '.csv')
 
-    return 
+def export(df, name):
+    route = cf.export_dir.replace('/App', '')
+    df.to_csv(route + str(name) + '.csv')
+
+    return None
+
+
+#    (_    ,_,    _)
+#    / `'--) (--'` \
+#   /  _,-'\_/'-,_  \
+#  /.-'     "     '-.\
