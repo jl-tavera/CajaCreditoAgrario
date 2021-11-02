d4a3316f78dbb7ac77862ad61a19b0ecf4df3893
jl-tavera <jl.tavera@uniandes.edu.co>
2021-11-02 15:18:07 -0500

    1960 clean

--- App/panel.py
+++ App/panel.py
@@ -12,7 +12,7 @@ year_1956 = fx.loadCleanYearCSV('DANE/1956_clean.csv')
 year_1957 = fx.loadCleanYearCSV('DANE/1957_clean.csv')
 year_1958 = fx.loadCleanYearCSV('DANE/1958_clean.csv')
 year_1959 = fx.loadCleanYearCSV('DANE/1959_clean.csv')
-
+year_1960 = fx.loadCleanYearCSV('DANE/1960_clean.csv')
 
 merge_1 = fx.mergeYears(year_1952, year_1953)
 merge_2 = fx.mergeYears(merge_1, year_1954)
@@ -21,6 +21,7 @@ merge_4 = fx.mergeYears(merge_3, year_1956)
 merge_5 = fx.mergeYears(merge_4, year_1957)
 merge_6 = fx.mergeYears(merge_5, year_1958)
 merge_7 = fx.mergeYears(merge_6, year_1959)
+merge_8 = fx.mergeYears(merge_7, year_1960)
 
 
-fx.exportPanel(merge_7, 'Panel')
+fx.exportPanel(merge_8, 'Panel')
