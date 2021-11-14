db2aa151f0ea7fdddcd59b2e6b3143dde57d76df
jl-tavera <jl.tavera@uniandes.edu.co>
2021-11-14 02:56:33 -0500

    Panel Real Values

--- data/panel.py
+++ data/panel.py
@@ -4,18 +4,19 @@ import pandas as pd
 import config as cf
 
 '''
-Nominal Panel Data
+REAL PANEL DATA
 '''
 
-year_1952 = fx.loadCleanYearCSV('DANE/1952_clean.csv')
-year_1953 = fx.loadCleanYearCSV('DANE/1953_clean.csv')
-year_1954 = fx.loadCleanYearCSV('DANE/1954_clean.csv')
-year_1955 = fx.loadCleanYearCSV('DANE/1955_clean.csv')
-year_1956 = fx.loadCleanYearCSV('DANE/1956_clean.csv')
-year_1957 = fx.loadCleanYearCSV('DANE/1957_clean.csv')
-year_1958 = fx.loadCleanYearCSV('DANE/1958_clean.csv')
-year_1959 = fx.loadCleanYearCSV('DANE/1959_clean.csv')
-year_1960 = fx.loadCleanYearCSV('DANE/1960_clean.csv')
+year_1952 = fx.loadCleanYearCSV('DANE/real/1952_r_clean.csv')
+year_1953 = fx.loadCleanYearCSV('DANE/real/1953_r_clean.csv')
+year_1954 = fx.loadCleanYearCSV('DANE/real/1954_r_clean.csv')
+year_1955 = fx.loadCleanYearCSV('DANE/real/1955_r_clean.csv')
+year_1956 = fx.loadCleanYearCSV('DANE/real/1956_r_clean.csv')
+year_1957 = fx.loadCleanYearCSV('DANE/real/1957_r_clean.csv')
+year_1958 = fx.loadCleanYearCSV('DANE/real/1958_r_clean.csv')
+year_1959 = fx.loadCleanYearCSV('DANE/real/1959_r_clean.csv')
+year_1960 = fx.loadCleanYearCSV('DANE/real/1960_r_clean.csv')
+year_1961 = fx.loadCleanYearCSV('DANE/real/1961_r_clean.csv')
 
 merge_1 = fx.mergeYears(year_1952, year_1953)
 merge_2 = fx.mergeYears(merge_1, year_1954)
@@ -26,7 +27,7 @@ merge_6 = fx.mergeYears(merge_5, year_1958)
 merge_7 = fx.mergeYears(merge_6, year_1959)
 merge_8 = fx.mergeYears(merge_7, year_1960)
 
-fx.exportPanel(merge_8, 'Panel')
+fx.exportPanel(merge_8, 'panel')
 
 '''
 Real Panel Data
